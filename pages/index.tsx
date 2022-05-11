27bf009083740ce7e13b8b7e2ade82621e9fbe32
Severyn Kurach <severynson.kurach@gmail.com>
2022-05-11 09:25:50 +0300

    Statefull components (10% DONE)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Button, Htag } from "../Components";
+import { Button, Htag, P, Tag } from "../Components";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -13,6 +13,13 @@ export default function Home() {
       <Button appearance="ghost" arrow="down">
         Button
       </Button>
+      <P size="l">Big</P>
+      <P size="m">Medium</P>
+      <P size="s">Small</P>
+      <Tag size="s">Ghost</Tag>
+      <Tag size="m" color="red">Red</Tag>
+      <Tag size="m" color='green'>Green</Tag>
+      <Tag size="m" color="primary">Green</Tag>
     </>
   );
 }
